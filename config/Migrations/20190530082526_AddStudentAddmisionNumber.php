716a2a88d70c347f197d6104e088933392964988
nizar <nizarucsc@gmail.com>
2019-06-03 13:29:56 +0530

    add migration file

---
+++ config/Migrations/20190530082526_AddStudentAddmisionNumber.php
@@ -0,0 +1,41 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddStudentAddmisionNumber extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $table1 = $this->table('institution_student_admission');
+        $table1
+            ->addColumn('admission_id', 'integer', [
+                'default' => 0,
+                'null' => true,
+            ]);
+        $table2 = $this->table('institution_students');
+        $table2
+            ->addColumn('admission_id', 'integer', [
+                'default' => 0,
+                'null' => true,
+            ]);
+
+    }
+
+    public function down()
+    {
+        $table1 = $this->table('institution_student_admission');
+        $table1
+            ->removeColumn('admission_id');
+
+        $table1 = $this->table('institution_students');
+        $table1
+            ->removeColumn('admission_id');
+
+    }
+}
--- plugins/OpenEmis/src/Template/Element/navigation.ctp
+++ plugins/OpenEmis/src/Template/Element/navigation.ctp
@@ -8,12 +8,23 @@ $selectedLink = '';
 if (isset($ControllerAction) && array_key_exists('selectedLink', $ControllerAction)) {
 	$selectedLink = implode('-', $ControllerAction['selectedLink']);
 }
+$institutionId = $this->Session->read('Institution.Institutions.id');
+$userId = $this->request->cookies['csrfToken'];
+
 ?>
 
 <div class="left-menu">
 	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		<?php echo $this->Navigation->render($navigations) ?>
 	</div>
+	<div class="left-menu">
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+			<ul id="nav-menu-1" class="nav nav-level-1 collapse in" role="tabpanel" data-level="1">
+		<li><a  href="https://dashboard.sis.moe.gov.lk/dashboard/script/principal_dashboard.js?token=<?php echo $userId;?>&id=<?php echo $institutionId; ?>" id="Institutions-dashboard" target="_blank"><span><i class="fa kd-reports"></i></span><b>Dashboard</b></a></li>
+		</ul>
+	</div>
+</div>
+
 </div>
 
 <script type="text/javascript">
