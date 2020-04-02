597a33cd25b7200f847481771ed434f14ab85d00
Mohamed Nizar <nizarucsc@gmail.com>
2020-04-02 13:47:26 +0530

    fix cache issue after upgrdae

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -84,7 +84,7 @@ try {
 try {
     Configure::load('datasource', 'default');
     Configure::load('app_extra', 'default');
-    ConnectionManager::setConfig(Configure::consume('Datasources'));
+    ConnectionManager::config(Configure::consume('Datasources'));
     // if (!Configure::read('Application.private.key') || !Configure::read('Application.public.key')) {
     //     throw new Exception('Could not load application key, please contact administrator to have the key set up for your application.');
     // }
