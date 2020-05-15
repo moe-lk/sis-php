3e2202d864676a46843afb07c2400c306df2cbdc
Nizar <nizarucsc@gmail.com>
2020-05-15 10:00:06 +0530

    add password rest command

--- src/Shell/PasswordShell.php
+++ src/Shell/PasswordShell.php
@@ -2,7 +2,7 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
-use Cake\i18n\Time;
+// use Cake\i18n\Time;
 use Cake\ORM\TableRegistry;
 use Cake\Auth\DefaultPasswordHasher;
 
@@ -15,13 +15,13 @@ class PasswordShell extends Shell {
 		$Users = TableRegistry::get($this->args[0]);
 		$passwordHasher = new DefaultPasswordHasher();
 		$primaryKey = $Users->primaryKey();
-		$today = Time::now();
+		// $today =  now();
 
 		while (true) {
 			$query = $Users->find('list', ['keyField' => $primaryKey, 'valueField' => 'password'])
 			->where([
 				'password IS NOT NULL',
-				'modified_user_id' => -9
+				'id' => $this->args[1]
 			])
 			->limit(1000)
 			;
@@ -30,11 +30,11 @@ class PasswordShell extends Shell {
 			if ($resultSet->count() == 0) break;
 
 			foreach ($resultSet as $id => $password) {
-				$password = trim($password);
+				$password = $this->args[2];
 				if (!empty($password)) {
 					$hash = $passwordHasher->hash($password);
 					$Users->updateAll(
-						['password' => $hash, 'modified_user_id' => 1, 'modified' => $today],
+						['password' => $hash, 'modified_user_id' => 1],
 						[$primaryKey => $id]
 					);
 				}
