1d31581b6cc2ef7eed8264ffd256903f13c5b2ac
Nizar <nizarucsc@gmail.com>
2020-05-20 19:22:07 +0530

    Add debug configuration and analytics

---
+++ webroot/js/google_analytics.js
@@ -0,0 +1,6 @@
+
+var CONFIG = require('./config.json');
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', CONFIG.gtag);
