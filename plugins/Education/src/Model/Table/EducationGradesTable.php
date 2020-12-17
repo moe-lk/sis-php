b892af3f9e2652c25fe2492cdb191aa3eb18f063
Mohamed Nizar <nizaucsc@gmail.com>
2020-12-17 09:33:19 +0530

    enable all next programme grades

--- plugins/Education/src/Model/Table/EducationGradesTable.php
+++ plugins/Education/src/Model/Table/EducationGradesTable.php
@@ -159,10 +159,12 @@ class EducationGradesTable extends ControllerActionTable
             // Default is to get the list of grades with the next programme grades
             if ($getNextProgrammeGrades) {
                 if ($firstGradeOnly) {
-                    $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextProgrammeFirstGradeList($programmeId);
-                } else {
                     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextGradeList($programmeId);
-                }
+
+                //     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextProgrammeFirstGradeList($programmeId);
+                // } else {
+                //     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextGradeList($programmeId);
+                // }
                 $results = $gradeOptions + $nextProgrammesGradesOptions;
             } else {
                 $results = $gradeOptions;
