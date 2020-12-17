66d838c56eac9b67162fd9f01d19375085a238a4
Mohamed Nizar <nizaucsc@gmail.com>
2020-12-17 09:43:02 +0530

    fix formate issue

--- plugins/Education/src/Model/Table/EducationGradesTable.php
+++ plugins/Education/src/Model/Table/EducationGradesTable.php
@@ -160,11 +160,9 @@ class EducationGradesTable extends ControllerActionTable
             if ($getNextProgrammeGrades) {
                 if ($firstGradeOnly) {
                     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextGradeList($programmeId);
-
                 //     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextProgrammeFirstGradeList($programmeId);
                 // } else {
-                //     $nextProgrammesGradesOptions = TableRegistry::get('Education.EducationProgrammesNextProgrammes')->getNextGradeList($programmeId);
-                // }
+                }
                 $results = $gradeOptions + $nextProgrammesGradesOptions;
             } else {
                 $results = $gradeOptions;
