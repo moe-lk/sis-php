4f301068803e7181c4883959c9e81c7c6a1c7f4c
HarindaVithana <harindavithana@gmail.com>
2021-06-21 15:57:45 +0530

    Update ExaminationCentresExaminationsInstitutionsTable.php

--- plugins/Examination/src/Model/Table/ExaminationCentresExaminationsInstitutionsTable.php
+++ plugins/Examination/src/Model/Table/ExaminationCentresExaminationsInstitutionsTable.php
@@ -10,6 +10,8 @@ class ExaminationCentresExaminationsInstitutionsTable extends Table
     {
         $this->table('examination_centres_examinations_institutions');
         $this->belongsTo('Examination', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
+        $this->belongsTo('ExaminationCentres', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
+
 
         parent::initialize($config);
     }
