33ca82d3473af6179d19e4fbc8f60b40e731e817
HarindaVithana <harindavithana@gmail.com>
2021-06-21 17:26:18 +0530

    Update InstitutionExaminationsTable.php

--- plugins/Institution/src/Model/Table/InstitutionExaminationsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionExaminationsTable.php
@@ -21,7 +21,7 @@ class InstitutionExaminationsTable extends ControllerActionTable
         $this->hasMany('ExaminationItems', ['className' => 'Examination.ExaminationItems', 'dependent' => true, 'cascadeCallbacks' => true]);
         $this->hasMany('ExaminationItemResults', ['className' => 'Examination.ExaminationItemResults', 'dependent' => true, 'cascadeCallbacks' => true]);
 //         $this->belongsTo('ExaminationCentres', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
-        $this->belongsTo('Examination', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
+//         $this->belongsTo('Examination', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
         $this->belongsToMany('ExaminationCentres', [
             'className' => 'Examination.ExaminationCentres',
             'joinTable' => 'examination_centres_examinations',
