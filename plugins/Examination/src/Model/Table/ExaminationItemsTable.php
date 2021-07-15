9e03fdc96c55817e7c58cb5a05c5bdb0a89e245e
Salman Salay <salmans@opensource.lk>
2021-07-15 18:27:32 +0530

    update examination items

--- plugins/Examination/src/Model/Table/ExaminationItemsTable.php
+++ plugins/Examination/src/Model/Table/ExaminationItemsTable.php
@@ -36,29 +36,62 @@ class ExaminationItemsTable extends Table
             'dependent' => true
         ]);
 
-        $this->hasMany('ExaminationSchoolStudents', ['className' => 'Examination.ExaminationStudents', 'saveStrategy' => 'replace', 'cascadeCallbacks' => true]);
+        public function implementedEvents()
+    {
+        $events = parent::implementedEvents();
+      $events['Model.ExaminationStudents.afterSave'] = 'ExaminationStudentsAfterSave';
+     // $events['ControllerAction.Model.afterAction'] = ['callable' => 'deleteAfterAction', 'priority' => 10];
+        return $events;
+    }
 
-       //add behaviour
+    public function beforeAction(Event $event, ArrayObject $extra){
 
-        $this->addBehavior('Restful.RestfulAccessControl',[
+        $institutionId = $this->Session->read('Institution.Institutions.id');
+        $extra['institution_id'] = $institutionId;
+        $academicPeriodOptions = $this->getAcademicPeriodOptions($institutionId);
+        $selectedAcademicPeriodId = $this->AcademicPeriods->getCurrent();
 
-            //ExaminationSchoolStudents=> ['view', 'edit']
-        ]);
+        if ($this->action == 'index') {
+            if (empty($query['academic_period_id'])) {
+                $query['academic_period_id'] = $this->AcademicPeriods->getCurrent();
+            }
 
+        }
+
+        if (array_key_exists($this->alias(), $this->request->data)) {
+            $selectedAcademicPeriodId = $this->postString('academic_period_id', $academicPeriodOptions);
+        } elseif ($this->action == 'edit' && isset($this->request->pass[1])) {
+            $id = $this->paramsDecode($this->request->pass[1]);
+            if ($this->exists($id)) {
+                $selectedAcademicPeriodId = $this->get($id)->academic_period_id;
+            }
+        }
+
+        $extra['selectedAcademicPeriodId'] = $selectedAcademicPeriodId;
+
+        $this->field('modified', ['visible' => false]);
+        $this->field('created_user_id', ['visible' => false]);
+        $this->field('created', ['visible' => false]);
+        $this->field('modified_user_id', ['visible' => false]);
+
+        $this->field('academic_period', ['type' => 'integer', 'visible' => ['view' => true]]);
+
+        $this->field('nsid',['visible'=>true]);
+        $this->field('f_name',['visible'=>true]);
+        $this->field('gender',['visible'=>true]);
+        $this->field('medium',['visible'=>true]);
+        $this->field('b_date',['visible'=>true]);
+        $this->field('user_identities',['visible'=>true]);
         
 
-/*
-        $this->field('students', [
-            'label' => '',
-            'override' => true,
-            'type' => 'element',
-            'element' => 'Examinations.Students/students',
-            'data' => [
-                'students' => [],
-                'studentOptions' => []
-            ],
-            'visible' => ['view' => true, 'edit' => true]
-        ]);
-        */
+        $this->setFieldOrder([
+            'nsid','f_name', 'gender','medium','b_date', 'user_identities']);
+
+        if ($this->action != 'index') {
+            if (isset($extra['toolbarButtons']['export'])) {
+                unset($extra['toolbarButtons']['export']);
+            }
+        }
+    
     }
 }
