9a2187015fa4215b5f8f24f073bb9a02ba579fde
inzamshahib <91459399+inzamshahib@users.noreply.github.com>
2022-04-02 11:34:09 +0530

    Update ExaminationCentresExaminationsSubjectsTable.php

--- plugins/Examination/src/Model/Table/ExaminationCentresExaminationsSubjectsTable.php
+++ plugins/Examination/src/Model/Table/ExaminationCentresExaminationsSubjectsTable.php
@@ -9,7 +9,7 @@ class ExaminationCentresExaminationsSubjectsTable extends Table
     public function initialize(array $config)
     {
         $this->table('examination_centres_examinations_subjects');
-        $this->belongsTo('Examination', ['className' => 'Examnation.Centers', 'foreignKey' => 'examination_centre_id']);
+        $this->belongsTo('Examination', ['className' => 'Examination.ExaminationCentres', 'foreignKey' => 'examination_centre_id']);
 
         parent::initialize($config);
     }
